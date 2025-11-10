6345e998a42c783aa2c12affd0b181eab1d32fd1
Danushri Prakash <danushri.saranyaprakash@gmail.com>
2025-11-10 19:17:07 +0530

    feat: Complete code quality setup with TypeScript, ESLint, Prettier, and Husky

---
+++ .husky/_/husky.sh
@@ -0,0 +1,31 @@
+﻿#!/usr/bin/env sh
+if [ -z \"\\" ]; then
+    debug() { :; }
+else
+    debug() { echo \"husky: \\"; }
+fi
+
+command_exists() {
+    command -v \"\\" >/dev/null 2>&1
+}
+
+load_user_script() {
+    local script=\"\\"
+    if [ -f \"\\" ]; then
+        debug \"running \\"
+        . \"\\"
+    else
+        debug \"skipping \ (not found)\"
+    fi
+}
+
+main() {
+    if [ -z \"\\" ]; then
+        load_user_script \"\\"
+        for hook in \"\/\"/??-*; do
+            load_user_script \"\\"
+        done
+    fi
+}
+
+main
